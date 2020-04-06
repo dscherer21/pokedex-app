6735c248f675b352a91a345f16ff5185a7672e7c
David Scherer-ODell <dscherer21@gmail.com>
2020-04-05 21:10:08 -0500

    creating server and react app base

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pokedex-app
 A Pokedex Application.
+
+Deployed at:
+
+Technologies used:
+
+To start server:
+
+To start client:
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+
+//Get request for my landing page
+app.get('/', (req, res) => {
+  res.json(JSON.parse(response.body));
+})
+
+//if running on a deployed site use the build folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> Server now listening on PORT ${PORT}!`);
+});
