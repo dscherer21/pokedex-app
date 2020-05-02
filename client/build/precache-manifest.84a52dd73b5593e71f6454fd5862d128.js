8fffeae917e02bd975320d4d33661b4dbfa60128
David Scherer-ODell <dscherer21@gmail.com>
2020-05-01 22:59:47 -0500

    adding basic stylings and google fonts

---
+++ client/build/precache-manifest.84a52dd73b5593e71f6454fd5862d128.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c666c70a78858dfac4a59aeaa59322c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1093ad949a60eebf09ab",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "f1506ddd16a7256a4abe",
+    "url": "/static/css/main.0523b5d3.chunk.css"
+  },
+  {
+    "revision": "1093ad949a60eebf09ab",
+    "url": "/static/js/2.903016eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.903016eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1506ddd16a7256a4abe",
+    "url": "/static/js/main.e11cd15a.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
