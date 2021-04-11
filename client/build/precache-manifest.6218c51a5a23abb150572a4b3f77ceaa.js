03bdff9131af702c05363aace9163712665393fd
David Scherer-ODell <dscherer21@gmail.com>
2021-04-11 01:48:56 -0500

    updating node_modules to fix server crash error

---
+++ client/build/precache-manifest.6218c51a5a23abb150572a4b3f77ceaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460a002e969999568fbda0f3dc757935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0819cd80cdb6e92cef18",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "c691a432124438618ef3",
+    "url": "/static/css/main.389b5a36.chunk.css"
+  },
+  {
+    "revision": "0819cd80cdb6e92cef18",
+    "url": "/static/js/2.c0aa7c20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c0aa7c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c691a432124438618ef3",
+    "url": "/static/js/main.6499a2d0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
